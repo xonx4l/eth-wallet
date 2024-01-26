f0ad3e1a94d27cd71d2ced6b6a342c32b8a5bcd7
xonx4l <sk797119@gmail.com>
2024-01-26 15:06:06 +0530

    update

--- src/simple_transaction.rs
+++ src/simple_transaction.rs
@@ -13,4 +13,23 @@ async fn main() -> Result<()> {
     let ganache = Ganache::new().mnemonic(mnemonic).spawn();
     println!("HTTP Endpoint : {}", ganache.endpoint());
     
+    let wallet: LocalWallet = ganache.keys()[0].clone().into();
+    let first_address = wallet.address();
+    println!(
+        "wallet first address: {}",
+        first_address.encode_hex::<String>()
+    );
+
+    let provider = Provider::try_from(ganache.endpoint())?.interval(Duration::from_millis(10));
+    
+    let first_balance = provider.get_balance(first_address, None).await?;
+    println!("wallet first balance: {}", first_balance);
+
+    let other_address_hex = "0xaf206dCE72A0ef76643dfeDa34DB764E2126E646";
+    let other_address = "0xaf206dCE72A0ef76643dfeDa34DB764E2126E646".parse::<Address>()?;
+    let other_balance = provider.get_balance(other_address, None).await?;
+    println!(
+        "Balance for address {}: {}",
+        other_address_hex, other_balance
+    );
 }
