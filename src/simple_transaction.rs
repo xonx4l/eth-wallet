523c37de02b6c6925376d629f810cec6de515f1b
xonx4l <sk797119@gmail.com>
2024-01-26 14:45:38 +0530

    create ganache endpoint

--- src/simple_transaction.rs
+++ src/simple_transaction.rs
@@ -1,1 +1,16 @@
 use::std::time::Duration;
+
+user ethers::{
+    prelude::{Address, LocalWallet, Middleware, Provider, Singer, TransactionRequest, U256},
+    utils::Ganache,
+};
+use eyre::{ContextCompat, Result};
+use hex::ToHex;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let mnemonic = "gas monster ski craft below illegal discover limit dig bundle bus artefact";
+    let ganache = Ganache::new().mnemonic(mnemonic).spawn();
+    println!("HTTP Endpoint : {}", ganache.endpoint());
+    
+}
